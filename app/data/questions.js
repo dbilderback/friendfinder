42f90462fe764944aba47903bf26bdc7348e63c4
dbilderback <dbilderback@gmail.com>
2018-02-26 16:18:14 -0500

    Questions for the survey

---
+++ app/data/questions.js
@@ -0,0 +1,17 @@
+var questionsArray = [
+    {
+        Question_1: "Your mind is always buzzing with unexplored ideas and plans?",
+        Question_2: "Generally speaking, you rely more on your experience than your imagination?",
+        Question_3: "You find it easy to stay relaxed and focused even when there is some pressure?",
+        Question_4: "You rarely do something just out of sheer curiosity?",
+        Question_5: "People can rarely upset you?",
+        Question_6: "It is often difficult for you to relate to other people’s feelings?",
+        Question_7: "In a discussion, truth should be more important than people’s sensitivities?",
+        Question_8: "You rarely get carried away by fantasies and ideas?",
+        Question_9: "You think that everyone’s views should be respected regardless of whether they are supported by facts or not?",
+        Question_10: "You feel more energetic after spending time with a group of people?"
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = questionsArray;
